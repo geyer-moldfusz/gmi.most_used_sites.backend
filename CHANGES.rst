58b6ac149166e3bb465197221a788b17d7a43e0f
Stefan <stefan@neuland.io>
2015-12-08 00:08:51 +0100

    prepare initial release

---
+++ CHANGES.rst
@@ -0,0 +1,7 @@
+Changelog for "Most used sites" backend
+=======================================
+
+0.1 (unreleased)
+----------------
+
+- Initial release
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import sys, os
 
-version = '0.1'
+version = '0.1.dev0'
 
 requires = [
     'colander',
@@ -14,19 +14,32 @@ requires = [
     'transaction',
     'zope.sqlalchemy',
     'waitress',
-    'webtest',
 ]
 
 setup(name='gmi.mostusedsites.backend',
       version=version,
-      description="REST backend for Most used Sites project",
+      description="REST backend for Most used sites project",
       long_description="""\
+REST backend for Most used sites project
+========================================
+
+Collects browsing behavior gathered by the Most used sites Firefox Add-On. It
+evaluates the data and presents them via a REST API.
 """,
-      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
-      keywords='',
+      classifiers=[
+          'Framework :: Pyramid',
+          'Intended Audience :: Education',
+          'Intended Audience :: Science/Research',
+          'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+          'Operating System :: POSIX :: Linux',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3.4',
+          'Topic :: Internet :: WWW/HTTP :: WSGI'
+      ],
+      keywords='geyer-moldfusz geyer moldfusz mostusedsites backend rest api',
       author='Stefan Walluhn, Arne Winter',
       author_email='stefan@neuland.io',
-      url='',
+      url='https://github.com/geyer-moldfusz/gmi.most_used_sites.backend',
       license='GPLv3',
       packages=['gmi', 'gmi.mostusedsites', 'gmi.mostusedsites.backend'],
       package_dir={'': 'src'},
